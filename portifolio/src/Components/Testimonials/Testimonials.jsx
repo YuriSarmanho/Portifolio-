a1f84ccc58be5b6b5fc30770148ecc3262c6ed29
YuriSarmanho <yuri.sarmanho@hotmail.com>
2022-05-25 23:05:16 -0300

    add swipe in testimonials

--- portifolio/src/Components/Testimonials/Testimonials.jsx
+++ portifolio/src/Components/Testimonials/Testimonials.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./Testimonials.css";
 import AVTR1 from "../../assets/avatar1.jpg";
 import AVTR2 from "../../assets/avatar2.jpg";
 import AVTR3 from "../../assets/avatar3.jpg";
 import AVTR4 from "../../assets/avatar4.jpg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
 
 const data = [
   {
@@ -38,19 +44,25 @@ const Testimonials = () => {
       <h5>Review from Clients</h5>
       <h2>Testimonials</h2>
 
-      <div className="container testimonials__container">
+      <Swiper
+        className="container testimonials__container"
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
         {data.map(({ avatar, name, review }, index) => {
           return (
-            <article className="testimonial">
+            <SwiperSlide className="testimonial">
               <div className="client__avatar">
                 <img src={avatar} alt="avatar one" />
               </div>
               <h5 className="client__name">{name}</h5>
               <small className="client__review">{review}</small>
-            </article>
+            </SwiperSlide>
           );
         })}
-      </div>
+      </Swiper>
     </section>
   );
 };
