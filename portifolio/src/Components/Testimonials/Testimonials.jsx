4fa64c944ac02ec2ae77fa4617841e09bfc7ce65
YuriSarmanho <yuri.sarmanho@hotmail.com>
2022-05-25 21:02:46 -0300

    testimonials data

--- portifolio/src/Components/Testimonials/Testimonials.jsx
+++ portifolio/src/Components/Testimonials/Testimonials.jsx
@@ -44,9 +44,9 @@ const Testimonials = () => {
             <article className="testimonial">
               <div className="client__avatar">
                 <img src={avatar} alt="avatar one" />
-                <h5 className="client__name">{name}</h5>
-                <small className="client__review">{review}</small>
               </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">{review}</small>
             </article>
           );
         })}
