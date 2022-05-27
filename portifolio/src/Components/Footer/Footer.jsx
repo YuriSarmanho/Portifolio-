d3c990204bb97305d1e1f0be2ca29d2ab600a6e7
Yuri Sarmanho <yuri.sarmanho@hotmail.com>
2022-05-27 11:48:47 -0300

    footer section added

--- portifolio/src/Components/Footer/Footer.jsx
+++ portifolio/src/Components/Footer/Footer.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import {FaFacebookF} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
+import {AiOutlineLinkedin} from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>EGATOR</a>
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#service">Service</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="http://facebook.com"></a>
+        <a href="http://instagram.com"></a>
+        <a href="http://linkedin.com"></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy;EGATOR tutorials. All right reserved.</small>
+      </div>
+    </footer>
   )
 }
 
